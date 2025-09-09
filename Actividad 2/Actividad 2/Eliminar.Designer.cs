d332808983259e1abeb2fad05aa15d4a1a225577
Nicolas Mares <nikomares04@gmail.com>
2025-09-09 18:21:28 -0300

    Agregue Form de Eliminacion

---
+++ Actividad 2/Actividad 2/Eliminar.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Actividad_2
+{
+    partial class Eliminar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_Eliminar = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btn_eliminar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_Eliminar
+            // 
+            this.lbl_Eliminar.AutoSize = true;
+            this.lbl_Eliminar.Location = new System.Drawing.Point(85, 47);
+            this.lbl_Eliminar.Name = "lbl_Eliminar";
+            this.lbl_Eliminar.Size = new System.Drawing.Size(168, 13);
+            this.lbl_Eliminar.TabIndex = 0;
+            this.lbl_Eliminar.Text = "SELECCIONAR PARA ELIMINAR";
+            this.lbl_Eliminar.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(110, 92);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // btn_eliminar
+            // 
+            this.btn_eliminar.Location = new System.Drawing.Point(125, 149);
+            this.btn_eliminar.Name = "btn_eliminar";
+            this.btn_eliminar.Size = new System.Drawing.Size(76, 25);
+            this.btn_eliminar.TabIndex = 3;
+            this.btn_eliminar.Text = "Eliminar";
+            this.btn_eliminar.UseVisualStyleBackColor = true;
+            // 
+            // Eliminar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(342, 238);
+            this.Controls.Add(this.btn_eliminar);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.lbl_Eliminar);
+            this.Name = "Eliminar";
+            this.Text = "Eliminar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_Eliminar;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btn_eliminar;
+    }
+}
