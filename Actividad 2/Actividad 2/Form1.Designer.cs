5426bc70dc4e3e0c3377413cbd82f2c85646fa48
aledesma <alejandroledesmahernan@gmail.com>
2025-09-10 19:56:53 -0300

    junte los dos proyectos en uno, anda el listar, eliminar y editar, se aplicaron estilos, creacion de emergente para edita, etc

--- Actividad 2/Actividad 2/Form1.Designer.cs
+++ Actividad 2/Actividad 2/Form1.Designer.cs
@@ -75,7 +75,6 @@
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Size = new System.Drawing.Size(800, 24);
             this.menuStrip1.TabIndex = 0;
-            this.menuStrip1.Text = "menuStrip1";
             // 
             // lista_articulo
             // 
